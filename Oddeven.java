b1500cd94bb8d9caffba4ecf0aed84685cb05be7
mohankondhana <mohankondhana@gmail.com>
2016-10-16 23:02:23 +0530

    oddeven

---
+++ Oddeven.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Oddeveen{
+
+   
+public static void main(String []args){
+     
+    int c;
+     
+System.out.println("enter the number");
+        
+Scanner in=new Scanner(System.in);
+        
+c=in.nextInt();
+       
+ if(c%2==0)
+       
+ System.out.println("even number");
+        
+else 
+         
+System.out.println("Odd number");
+        
+     
+}
+}
